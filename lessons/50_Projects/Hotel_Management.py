9e5b7633ad00e4f7bb6a190d3427d90fc82d0182
samyrq0810 <samyrq0810@gmail.com>
2025-12-20 22:30:29 +0000

    qwertyuiop

--- lessons/50_Projects/Hotel_Management.py
+++ lessons/50_Projects/Hotel_Management.py
@@ -3,6 +3,13 @@ if needed later on, check in rooms are in this order: [room type, number of days
 """
 
 import random
+from guizero import App, Text, PushButton, Slider, Box
+
+roomStandardNum = 0
+roomLuxuryNum = 0
+roomSuiteNum = 0
+
+Bank = 10000
 
 def room_type():
     Room_Type_Numify = random.randint(0, 3)
@@ -47,8 +54,29 @@ def check_in():
 
     return checkIn
 
+def build_standard(money):
+    cost = 2000
+    if money > cost:
+        roomStandardNum = roomStandardNum + 1
+        money = money - 2000
 
-print(check_in())
+def build_luxury(money):
+    cost = 3000
+    if money > cost:
+        roomLuxuryNum = roomLuxuryNum + 1
+        money = money - 3000
 
+def build_suite(money):
+    cost = 4000
+    if money > cost:
+        roomSuiteNum = roomSuiteNum + 1
+        money = money - 4000
 
-    
+app = App(title = "hotel management", layout = "grid")
+standardBox = Box(app, border = True, grid = [0,0])
+standard_room = Text(standardBox, text="number of standard rooms")
+luxury_room = Text(app, text="number of luxury rooms", grid = [1,0])
+
+luxuryBox = Box(app, border = True, grid = [3,0])
+app.display()
+
