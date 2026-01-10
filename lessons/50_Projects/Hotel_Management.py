57846869cc80fe60e1f72e706ba576cecbb6cad1
samyrq0810 <samyrq0810@gmail.com>
2026-01-10 22:30:00 +0000

    qwertyuiop

--- lessons/50_Projects/Hotel_Management.py
+++ lessons/50_Projects/Hotel_Management.py
@@ -4,73 +4,124 @@ if needed later on, check in rooms are in this order: [room type, number of days
 
 import random
 from guizero import *
+import time
 
-roomStandardNum = 0
-roomLuxuryNum = 0
-roomSuiteNum = 0
+# roomStandardNum = 0
+# roomLuxuryNum = 0
+# roomSuiteNum = 0
 
-Bank = 10000
+bank = 50000
 
-def room_type():
-    Room_Type_Numify = random.randint(0, 3)
-    Room_Type = ""
+# def room_type():
+#     Room_Type_Numify = random.randint(0, 3)
+#     Room_Type = ""
 
-    if Room_Type_Numify == 0:
-        Room_Type = "Standard"
-    elif Room_Type_Numify == 1:
-        Room_Type = "Luxury"
-    else:
-        Room_Type = "Suite"
+#     if Room_Type_Numify == 0:
+#         Room_Type = "Standard"
+#     elif Room_Type_Numify == 1:
+#         Room_Type = "Luxury"
+#     else:
+#         Room_Type = "Suite"
         
-    return Room_Type
+#     return Room_Type
 
-def number_of_days():
-    Num_of_days = random.randint(1, 6)
+# def number_of_days():
+#     Num_of_days = random.randint(1, 6)
 
-    return Num_of_days
+#     return Num_of_days
 
-def cost(room_type, number_of_days):
-    cost = 0
-    if room_type == "Standard":
-        cost = 100
-    elif room_type == "Luxury":
-        cost = 150
-    elif room_type == "Suite":
-        cost = 200
+# def cost(room_type, number_of_days):
+#     cost = 0
+#     if room_type == "Standard":
+#         cost = 100
+#     elif room_type == "Luxury":
+#         cost = 150
+#     elif room_type == "Suite":
+#         cost = 200
     
-    cost = cost * number_of_days
+#     cost = cost * number_of_days
 
-    return cost
+#     return cost
         
-def check_in():
-    roomType = room_type()
-    numberOfDays = number_of_days()
-    Cost = cost(roomType, numberOfDays)
-    checkIn = []
+# def check_in():
+#     roomType = room_type()
+#     numberOfDays = number_of_days()
+#     Cost = cost(roomType, numberOfDays)
+#     checkIn = []
 
-    checkIn.append(roomType)
-    checkIn.append(numberOfDays)
-    checkIn.append(Cost)
+#     checkIn.append(roomType)
+#     checkIn.append(numberOfDays)
+#     checkIn.append(Cost)
 
-    return checkIn
+#     return checkIn
 
-def build_standard(money):
-    cost = 2000
-    if money > cost:
-        roomStandardNum = roomStandardNum + 1
-        money = money - 2000
+# def build_standard(money):
+#     cost = 2000
+#     if money > cost:
+#         roomStandardNum = roomStandardNum + 1
+#         money = money - 2000
 
-def build_luxury(money):
-    cost = 3000
-    if money > cost:
-        roomLuxuryNum = roomLuxuryNum + 1
-        money = money - 3000
+# def build_luxury(money):
+#     cost = 3000
+#     if money > cost:
+#         roomLuxuryNum = roomLuxuryNum + 1
+#         money = money - 3000
 
-def build_suite(money):
-    cost = 4000
-    if money > cost:
-        roomSuiteNum = roomSuiteNum + 1
-        money = money - 4000
+# def build_suite(money):
+#     cost = 4000
+#     if money > cost:
+#         roomSuiteNum = roomSuiteNum + 1
+#         money = money - 4000
+
+
+
+class Room:
+   
+    def __init__(self, cost:int, roomNumber:int):
+        """Initializes a new Person object."""
+        self.cost = cost
+        self.isAvailable = True
+        self.roomNumber = roomNumber
+        self.guestName = ""
+        self.numDays = 0
+        self.isbuilt = False
+
+    def build_new_room():
+        x = 100
+        global bank
+        if bank >= 5000:
+            bank = bank - 5000
+
+
+    def book(self, guestName:str, numDays:int):
+        global bank
+        self.isAvailable = False
+        self.numDays = numDays
+        bank = bank + numDays * self.cost
+        self.guestName = guestName
+        print(f"{self.guestName} has booked room number {self.roomNumber} for {numDays} days")
+        
+    def check_out(self):
+        last_time = time.time()
+        days = 0
+
+        while days < self.numDays:
+            if time.time() - last_time > 5:
+                days = days + 1
+                last_time = time.time()
+
+        print(f"{self.guestName} has check out of room number {self.roomNumber}")
+        self.isAvailable = True
+
+
+
+rooms = [Room(125, i + 1) for i in range(100)]
+
+
+
+
+
+
 
 app = App(title="Hotel Management", layout="grid", height=400, width=400)
 
@@ -88,23 +139,4 @@ app.display()
 
 
 
-class Room:
-    def __init__(self, cost:int, roomNumber:int):
-        """Initializes a new Person object."""
-        self.cost = cost
-        self.isAvailable = True
-        self.roomNumber = roomNumber
-        self.guestName = ""
-        self.numDays = 0
-    def book(self, guestName:str, numDays:int):
-        self.isAvaible = False
-        self.numDays = numDays
-        print(f"{guestName} has booked room number {self.roomNumber} for {numDays} days")
 
-
-
-room1 = Room(125, 101)
-room2 = Room(125, 102)
-room3 = Room(125, 103)
-
-room1.book("Nick", "3")
