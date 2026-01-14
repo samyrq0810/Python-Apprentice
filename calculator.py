47de430a7613cdd939107b6bb156a79f8f4914dd
samyrq0810 <samyrq0810@gmail.com>
2026-01-14 03:32:14 +0000

    1234567890

--- calculator.py
+++ calculator.py
@@ -4,24 +4,17 @@ first_num = 0
 second_num = 0
 operation = ""
 
+def enterbutton_push():
+    global firstnumtextbox
+    global secondnumtextbox
+
+    return firstnumtextbox.value + secondnumtextbox.value
 
 
-app = App(title = "calculator", layout = "grid")
 
-button0 = PushButton(app, text="0", grid=[2,3], command=)
-button1 = PushButton(app, text="1", grid=[1,0])
-button2 = PushButton(app, text="2", grid=[2,0])
-button3 = PushButton(app, text="3", grid=[3,0])
-button4 = PushButton(app, text="4", grid=[1,1])
-button5 = PushButton(app, text="5", grid=[2,1])
-button6 = PushButton(app, text="6", grid=[3,1])
-button7 = PushButton(app, text="7", grid=[1,2])
-button8 = PushButton(app, text="8", grid=[2,2])
-button9 = PushButton(app, text="9", grid=[3,2])
+app = App(title = "calculator", layout = "grid", height=500, width=500)
 
-buttonAdd = PushButton(app, text="+", grid=[4,0])
-buttonSub = PushButton(app, text="-", grid=[4,1])
-buttonMult = PushButton(app, text="x", grid=[4,2])
-buttonDiv = PushButton(app, text="÷", grid=[4,3])
-
+firstnumtextbox = TextBox(app, width=350, grid=[1,3])
+secondnumtextbox = TextBox(app, width=350, grid=[1,6])
+enterbutton = PushButton(app, text="Enter", grid=[0,0], command=print(enterbutton_push()))
 app.display()
