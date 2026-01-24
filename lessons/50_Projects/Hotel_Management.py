ef164a007d5721e5588eb3a384515b5495f433d9
samyrq0810 <samyrq0810@gmail.com>
2026-01-24 22:30:38 +0000

    qwertyuiop

--- lessons/50_Projects/Hotel_Management.py
+++ lessons/50_Projects/Hotel_Management.py
@@ -1,77 +1,25 @@
 """
 if needed later on, check in rooms are in this order: [room type, number of days, cost].
+
+!!## kind of redoed the checkout funt in class room, may not work ##!!
 """
 
-import random
 from guizero import *
 import time
 
-# roomStandardNum = 0
-# roomLuxuryNum = 0
-# roomSuiteNum = 0
-
 bank = 50000
+numOfEmptyRoom = 25
+x = 0
+days = 0
 
-# def room_type():
-#     Room_Type_Numify = random.randint(0, 3)
-#     Room_Type = ""
+while x < 10:
+    last_time = time.time
 
-#     if Room_Type_Numify == 0:
-#         Room_Type = "Standard"
-#     elif Room_Type_Numify == 1:
-#         Room_Type = "Luxury"
-#     else:
-#         Room_Type = "Suite"
-        
-#     return Room_Type
-
-# def number_of_days():
-#     Num_of_days = random.randint(1, 6)
-
-#     return Num_of_days
-
-# def cost(room_type, number_of_days):
-#     cost = 0
-#     if room_type == "Standard":
-#         cost = 100
-#     elif room_type == "Luxury":
-#         cost = 150
-#     elif room_type == "Suite":
-#         cost = 200
+    if time.time() - last_time > 5:
+        days = days + 1
+        last_time = time.time()
+        x = x + 1
     
-#     cost = cost * number_of_days
-
-#     return cost
-        
-# def check_in():
-#     roomType = room_type()
-#     numberOfDays = number_of_days()
-#     Cost = cost(roomType, numberOfDays)
-#     checkIn = []
-
-#     checkIn.append(roomType)
-#     checkIn.append(numberOfDays)
-#     checkIn.append(Cost)
-
-#     return checkIn
-
-# def build_standard(money):
-#     cost = 2000
-#     if money > cost:
-#         roomStandardNum = roomStandardNum + 1
-#         money = money - 2000
-
-# def build_luxury(money):
-#     cost = 3000
-#     if money > cost:
-#         roomLuxuryNum = roomLuxuryNum + 1
-#         money = money - 3000
-
-# def build_suite(money):
-#     cost = 4000
-#     if money > cost:
-#         roomSuiteNum = roomSuiteNum + 1
-#         money = money - 4000
 
 
 
@@ -79,60 +27,58 @@ class Room:
    
     def __init__(self, cost:int, roomNumber:int):
         """Initializes a new Person object."""
+
         self.cost = cost
         self.isAvailable = True
         self.roomNumber = roomNumber
         self.guestName = ""
         self.numDays = 0
-        self.isbuilt = False
 
-    def build_new_room():
-        x = 100
-        global bank
-        if bank >= 5000:
-            bank = bank - 5000
-
-
+  
     def book(self, guestName:str, numDays:int):
         global bank
+        global numOfEmptyRoom
         self.isAvailable = False
         self.numDays = numDays
         bank = bank + numDays * self.cost
         self.guestName = guestName
+        numOfEmptyRoom = numOfEmptyRoom - 1
         print(f"{self.guestName} has booked room number {self.roomNumber} for {numDays} days")
-        
-    def check_out(self):
-        last_time = time.time()
-        days = 0
 
+    def checkout(self):
+        global days
         while days < self.numDays:
-            if time.time() - last_time > 5:
-                days = days + 1
-                last_time = time.time()
+            pass
 
         print(f"{self.guestName} has check out of room number {self.roomNumber}")
         self.isAvailable = True
+        numOfEmptyRoom = numOfEmptyRoom + 1
 
 
-
-rooms = [Room(125, i + 1) for i in range(100)]
-
-
-
+rooms = [Room(125, i + 1) for i in range(25)]
 
 
 
 
 app = App(title="Hotel Management", layout="grid", height=400, width=400)
 
-box1 = Box(app, width=100, height=300, grid=[0,0], border=True)
-box2 = Box(app, width=100, height=300, grid=[1,0], border=True)
-box3 = Box(app, width=100, height=300, grid=[2,0], border=True)
-box4 = Box(app, width=100, height=300, grid=[3,0], border=True)
+box1 = Box(app, width=200, height=100, grid=[0,0], border=True)
+box2 = Box(app, width=200, height=100, grid=[1,0], border=True)
 
-box5 = Box(app, width=400, height=100, grid=[0,0], border=True)
-box6 = Box(app, width=400, height=100, grid=[0,1], border=True)
-box7 = Box(app, width=400, height=100, grid=[0,2], border=True)
+numOfEmptyRooms = Text(box1, text=f"Number of empty rooms: {numOfEmptyRoom}")
+
+
+
+rooms[1].book("zxc",1)
+rooms[2].book("jh",2)
+rooms[3].book("sd",3)
+rooms[4].book("dgh",4)
+rooms[5].book("ew",5)
+rooms[6].book("qw",6)
+
+
+
+
 
 
 app.display()
